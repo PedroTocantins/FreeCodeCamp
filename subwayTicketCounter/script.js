19c404fe3836fc93c4e87cc3f1678c2169581015
Pedro Tocantis <pedro.tocantis@tandersolucoes.com>
2023-03-27 08:26:42 -0300

    Count Passages Commit

---
+++ subwayTicketCounter/script.js
@@ -0,0 +1,16 @@
+let count = 0;
+let countEl = document.getElementById("count-el");
+let previousEntries = document.getElementById("previous-entries")
+countEl.innerText = count;
+let previousEntriesArray = []
+
+
+function increment() {
+  count += 1;
+  countEl.innerText = count;
+};
+
+function save() {
+  previousEntries.innerText += count+'-';
+}
+
